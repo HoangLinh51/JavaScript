e042a3f52163eb43232375b9d4662bfe1b06fa79
HoangLinh51 <linhle5112002@gmail.com>
2022-09-08 17:19:00 +0700

    video9

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,16 @@
-console.log('Hello world from JavaScript')
-console.log('Hello world with ERIC')
+// console.log('Hello world from JavaScript')
+// console.log('Hello world with ERIC')
+
+// console.log(`Hello' '" ' world from javascript: `)
+// let x = 5, y = 6, z = 7;
+// console.log(" Test string", 'x= ', x, ' y= ', y, ' z=', z)
+
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5; 
+let b = 6;
+let c = 6.9
+let d = '5'
+let e = +d;
+let f = Number(d)
+console.log('e value: ', e, 'type: ', typeof a)
+console.log(a === e, a === d, 'a: ', typeof a, 'd: ', typeof d)
